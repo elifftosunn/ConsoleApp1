29cba28aa5ad0d336418d427632cdc6f599c65e6
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:18:55 +0300

    Update HousingLoanManager.cs

--- OOP3/HousingLoanManager.cs
+++ OOP3/HousingLoanManager.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOP3
 {
-    class HousingLoanManager : ICreditManager //Konut Kredisi
+    class HousingLoanManager : ICreditManager
     {
         public void BiseyYap()
         {
