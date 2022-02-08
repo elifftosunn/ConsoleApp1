a2353df9baa14be450d02c9f256d0c714798eb50
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 12:00:32 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public List<Customer> customer = new List<Customer>();
+        public void newCustomer(int id,string name, string surname,string email)
+        {
+            this.customer.Add(new Customer(id,name,surname,email));
+        }
+        public void deleteCustomer(int id)
+        {
+            Customer c = this.customer.Where(x => x.Id == id).FirstOrDefault();
+            if (c != null)
+            {
+                this.customer.Remove(c);
+                Console.WriteLine("\n"+id+" id'li müşteri silindi.\n");
+            }
+            else
+            {
+                throw new Exception("Silinecek müşteri bulunamadı.");
+            }
+        }
+        public void ListCustomer()
+        {
+            Console.WriteLine("Id".PadRight(10) + "Name".PadRight(10) + "SurName".PadRight(10) + "Email");
+            Console.WriteLine("".PadRight(45, '-'));
+            foreach (var item in customer)
+            {
+                Console.WriteLine(item.Id.ToString().PadRight(10) + item.Name.PadRight(10) + item.surName.PadRight(10) + item.email);
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customer = new CustomerManager();
+
+            Customer customer1 = new Customer() { Id = 1, Name = "Akın", surName = "Yılmaz", email="info@gmail.com" ,customerNo=10};
+            Customer customer2 = new Customer() { Id = 2, Name = "Deniz", surName = "Yılmaz", email="info1@gmail.com",customerNo=20 };
+            Customer customer3 = new Customer() { Id = 3, Name = "Selin", surName = "Yılmaz", email="info2@gmail.com" ,customerNo=30};
+            Customer customer4 = new Customer() { Id = 4, Name = "İrem", surName = "Yılmaz", email = "info3@gmail.com",customerNo=40 };
+
+            customer.newCustomer(customer1.Id,customer1.Name, customer1.surName, customer1.email);
+            customer.newCustomer(customer2.Id,customer2.Name, customer2.surName, customer2.email);
+            customer.newCustomer(customer3.Id,customer3.Name, customer3.surName, customer3.email);
+            customer.newCustomer(customer4.Id,customer4.Name, customer4.surName, customer4.email);
+            customer.ListCustomer();
+            customer.deleteCustomer(1);
+            customer.ListCustomer();
+
+        }
+    }
+}
