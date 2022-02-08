15f2244d4a0371bd976d8605349e2a05758b19dd
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 14:26:46 +0300

    GenericsIntro

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> 
+    {
+        T[] items; //T'nin tipi değişebilir
+        public MyList()  //constructor => class'ı new'lediğimiz zaman otomatik çalışır
+        {
+            items = new T[0];
+        }
+        public void Add(T item) //kullanıcı veri tipi olarak ne verirse onu alır
+        {
+            T[] tempArray = items; //gecici dizinin items'in referansını tutuyor gecici olarak
+            items = new T[items.Length+1]; //dizinin eleman sayısı 1 arttı
+            //items=tempArray;
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; //items emaneten verdiği elemanlarını geri alıyor
+            }
+            items[items.Length - 1] = item; //items'in son elemanına yeni elemanı ekledik
+        }
+        public int Length
+        {
+            get 
+            {
+                return items.Length;
+            }
+        }
+        public T[] Items
+        {
+            get
+            {
+                return items;
+            }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string> { };
+            names.Add("Elif");
+            Console.WriteLine(names.Length);
+            names.Add("Elif");
+            Console.WriteLine(names.Length);
+            names.Add("Elif");
+            Console.WriteLine(names.Length);
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+    }
+}
