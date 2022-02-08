9eb43c3353718a61f35a9f568317090fdb1ae445
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:16:25 +0300

    Update ApplicationManager.cs

--- OOP3/ApplicationManager.cs
+++ OOP3/ApplicationManager.cs
@@ -4,21 +4,16 @@ using System.Text;
 
 namespace OOP3
 {
-    class ApplicationManager //Başvuru İşlemleri
+    class ApplicationManager 
     {
-        //buraya hangi krediyi gönderirsem o kredi hesaplanır yani parametreye konut kredisi gönderirsem konut hesaplarnı,taşıt gönderirsem taşıt hesaplanır.
         public void makeApplication(ICreditManager creditManager,List<ILoggerService> loggerServices) //burada tek bir kredi istiyor
         {
-            //Başvuran bilgilerini değerlendirme
-            //Kredi Hesaplama
             creditManager.Calculate();
-            //loggerService.Log();  //loglama tek olduğu zaman
-            foreach (var loggerService in loggerServices) //birden fazla loglama olduğu zaman
+            foreach (var loggerService in loggerServices) 
             {
-                loggerService.Log(); // her bir loglayıcığı çalıştır dedik
+                loggerService.Log(); 
             }
         }
-        //kredi ön bilgilendirmesi
         public void creditPreNotification(List<ICreditManager> credits) //burada birden fazla kredi sunuyorsun 1 de olabilir 1000 de
         {
             foreach (var item in credits)
