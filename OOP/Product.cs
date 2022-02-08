e73505a1319782764a38a15437e4e5ed91185a4c
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:10:23 +0300

    Add files via upload

---
+++ OOP/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public double unitPrice { get; set; }
+        public int MyProperty { get; set; }
+        public int CategoryId { get; set; }
+        public int UnitsInStock { get; set; } //ürünün stok adedi
+    }
+}
---
+++ OOP/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.productName+" eklendi.");
+        }
+        public void UpDate(Product product)
+        {
+            Console.WriteLine(product.productName+" güncellendi.");
+        }
+        public int Topla(int s1, int s2)
+        {
+            return s1 + s2;
+        }
+        public void Topla2(int s1, int s2)
+        {
+            Console.WriteLine(s1+s2);
+        }
+        /*
+        public void Any(int number)
+        {
+            number = 99;
+        }*/
+    }
+}
