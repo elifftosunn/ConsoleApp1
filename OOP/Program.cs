3d0b12d3cbb49d9af80793f337b2f1541d1c5570
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:11:15 +0300

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -9,21 +9,12 @@ namespace OOP
             Product product1 = new Product() { Id=2,CategoryId=5,productName="Kalem",unitPrice=35,UnitsInStock=5};
             Product product2 = new Product() { Id=3,CategoryId=10,productName="Masa",unitPrice=500,UnitsInStock=3};
             ProductManager productManager = new ProductManager();
-            productManager.Add(product1);  //bellek
+            productManager.Add(product1);  
             Console.WriteLine(product1.productName);
-
-
-            /*
-            int number = 100; //value
-            productManager.Any(number);
-            Console.WriteLine(number);*/
 
             int sumResult=productManager.Topla(3, 5);
             Console.WriteLine(sumResult*2);
             //productManager.Topla2(3, 5);
-
-
-        
         }
     }
 }
