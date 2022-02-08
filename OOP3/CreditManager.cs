486503ef9506e3ac74d6e5447754026f4b554cf1
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:17:23 +0300

    Update CreditManager.cs

--- OOP3/CreditManager.cs
+++ OOP3/CreditManager.cs
@@ -4,14 +4,9 @@ using System.Text;
 
 namespace OOP3
 {
-    // okunurluğu arttırmak için interface'leri I harfi ile başlatırız.
-    // bankadaki her kredide olması gereken metotlar olduğundan ve her kredi için hesap farklı olduğundan interface olarak tanımlarız.
-
-    //interface'leri birbirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız.
-    interface ICreditManager //interface => şablon görevi görür
+    interface ICreditManager
     {
         void Calculate();
         void BiseyYap();
-        
     }
 }
