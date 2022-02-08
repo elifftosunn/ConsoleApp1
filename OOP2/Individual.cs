1243acfd1a6b8a8e5ff5a8601323f17d12ab1ef7
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 21:13:42 +0300

    Update Individual.cs

--- OOP2/Individual.cs
+++ OOP2/Individual.cs
@@ -4,10 +4,8 @@ using System.Text;
 
 namespace OOP2
 {
-    // Individual:Customer => miras(inheritance)
-    class Individual :Customer //Gerçek Müşteri de müşteridir demek
+    class Individual :Customer 
     {
-        //bir veri üzerinde matematiksel işlem yapmıyorsak() ona string,char atamak daha verimli bir sonuç verir.
         public string TcNumber { get; set; }
         public string CustomerName { get; set; }
         public string CustomerLastName { get; set; }
