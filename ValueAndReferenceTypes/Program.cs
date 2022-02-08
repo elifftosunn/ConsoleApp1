f8222afa3fe84d24a6be04d52f6cd9d9499a170a
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 15:19:45 +0300

    Update Program.cs

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -20,6 +20,51 @@ namespace ValueAndReferenceTypes
             numbers2[0] = 999;
             Console.WriteLine(numbers1[0]); //numbers1'in heap'teki yeri artık numbers2'nin heap'teki yeri olduğundan numbers2'nin degerlerini degistirdigimizde numbers1'in degerleri de degisecektir.
             Console.WriteLine(numbers1[0]+" "+numbers1[1]+" "+numbers1[2]);
+            
+            
+            
+            
+            
+            
+            Customer customer = new Customer();
+            customer.FirstName = "Ahmet";
+            customer.CreditCardNumber = "123456789";
+
+            Employee employee = new Employee();
+            employee.FirstName = "Irmak";
+            employee.EmployeeNumber = 42345356;
+            //base sınıfa(Person) onu inherit eden sınıfın referansları atanır.
+            Person person = customer;  // person customer'in adresini tutuyor.
+            customer.FirstName = "Ali";
+            Console.WriteLine(person.FirstName);
+            //ama biz customer özelliğini(CreditCardNumber) person'da göremiyoruz. Özelliğini görebilmemiz için =>
+            Console.WriteLine(((Customer)person).CreditCardNumber);
+            PersonManager personManager = new PersonManager();
+            personManager.Add(customer);
+            personManager.Add(employee);
+            personManager.Add(person);
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+    }
+    // BASE CLASS : Person 
+    class Customer : Person 
+    {
+        public string CreditCardNumber { get; set; }
+    }
+    class Employee : Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
         }
     }
 }
