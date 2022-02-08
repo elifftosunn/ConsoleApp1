79b28988af18f093ef3efc34c4544633f8d246f6
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 05:26:15 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,20 +6,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /*
-              Ctrl+K+C =>açıklama satırı
-              Ctrl+C =>kopyalama Ctrl+V =>Alt satıra ekleme Ctrl+Z =>Geri Alma
-
-            */
-
-            //"ternary operatörü" =>  Tek satırda hem kontrolü hem cevabı sağlayan bir koşul-şart operatörü.
-            int saat = 20;
-            string sonuc = (saat < 17) ? "İyi günler" : "İyi akşamlar";
-            Console.WriteLine(sonuc);
-
-            //switch-case => çok fazla kontrol edilmesi gereken şarta bağlı durumda if bloklarının yerine kullanılan bilen döngülerdir.
-
-            /*
             Courses course1 = new Courses();
             Courses course2 = new Courses();
             Courses course3 = new Courses();
@@ -37,7 +23,7 @@ namespace ConsoleApp1
             foreach (var item in courses)
             {
                 Console.WriteLine(item.CourseName + ": " + item.CourseTeacher + " " + item.IzlenmeOranı);
-            }*/
+            }
             Basketball b1 = new Basketball(){name="Ufuk",age=21,size=1.90 };
             Basketball b2 = new Basketball(){name="Cristiano",age=22,size=1.70 };
             Basketball b3 = new Basketball(){name="Ümit",age=23,size=1.87};
