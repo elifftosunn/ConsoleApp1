a1589f50dacda424f43b8c7f7418e444f9b25b68
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-08 16:41:18 +0300

    Add files via upload

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keys;
+        T2[] values;
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+        public void Add(T1 x,T2 y)
+        {
+            T1[] temp1 = keys;
+            T2[] temp2 = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                keys[i] = temp1[i]; 
+            }
+            for (int i = 0; i < temp2.Length; i++)
+            {
+                values[i] = temp2[i];
+            }
+            keys[keys.Length - 1] = x;
+            values[values.Length - 1] = y;
+        }
+        public int keyLength { get { return keys.Length; } }
+        public T1[] KeyItems { get { return keys; } }
+        public int valueLength { get { return values.Length; } }
+        public T2[] ValueItems { get { return values; } }
+        public void Value(int key)
+        {
+            Console.WriteLine(values[key]);
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Elif");
+            myDictionary.Add(2, "Selen");
+            myDictionary.Add(3, "Deniz");
+            myDictionary.Add(4, "Ahmet");
+            for (int i = 0; i < myDictionary.keyLength; i++)
+            {
+                myDictionary.Value(i);
+            }
+
+        }
+    }
+}
